d69c683189775323bf493bddfb7585be7b7b60a8
Confoederatio <htmlprogrammingbn@gmail.com>
2025-04-28 05:55:24 +0100

    Update startup.js

--- js/pages/homepage/startup.js
+++ js/pages/homepage/startup.js
@@ -76,7 +76,7 @@ all_bodies[0].remove();
 
 //Mobile temporary fix
 if (isMobileDevice()) {
-  window.alert(`You are on a mobile device.\n\nThis website is currently unoptimised for mobile devices. Please use a desktop computer to view this website.\n\nExperimental Patch: 003`);
+  window.alert(`You are on a mobile device.\n\nThis website is currently unoptimised for mobile devices. Please use a desktop computer to view this website. Experimental Patch: 003`);
 
   document.getElementById("about-me-section").setAttribute("class",
     document.getElementById("about-me-section").getAttribute("class") + " display-none"

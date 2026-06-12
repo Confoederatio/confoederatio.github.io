f5d8c6059dd0d287e4716198160c5633da4d907d
Vis Tacitus <vistacitus@confoederatio.org>
2026-06-12 02:41:46 +0000

    fix imports

--- js/config/config_backend.js
+++ js/config/config_backend.js
@@ -1,48 +1,6 @@
 window.config = {}; //[WIP] - Config
 config.homepage = {
-  js_files: [
-    //Icoemi
-    "js/UF/icoemi/icoemi.js",
-    
-    //Head files
-    "js/UF/animation/basic_animation.js",
-    "js/UF/arrays/basic_arrays.js",
-    "js/UF/arrays/conversion_arrays.js",
-    "js/UF/arrays/dataframes_arrays.js",
-    "js/UF/arrays/maths_arrays.js",
-    "js/UF/arrays/search_sort_arrays.js",
-    "js/UF/BrowserUI/basic_html.js",
-    "js/UF/BrowserUI/context_menus.js",
-    "js/UF/BrowserUI/groups_framework.js",
-    "js/UF/BrowserUI/hierarchy_framework.js",
-    "js/UF/BrowserUI/WebComponent.js",
-    "js/UF/class/class_basic.js",
-    "js/UF/colours/basic_colours.js",
-    "js/UF/date/basic_date.js",
-    "js/UF/date/history_date.js",
-    "js/UF/log/basic_log.js",
-    "js/UF/numbers/basic_numbers.js",
-    "js/UF/numbers/ranges_numbers.js",
-    "js/UF/objects/basic_objects.js",
-    "js/UF/objects/maths_objects.js",
-    "js/UF/objects/smart_search_objects.js",
-    "js/UF/pathfinding/a_star_pathfinding.js",
-    "js/UF/strings/basic_strings.js",
-    "js/UF/strings/cleaning_strings.js",
-    "js/UF/strings/split_strings.js",
-    
-    //Viewport 1
-    "js/pages/homepage/viewport_1/HomepageBanner.js",
-    
-    //Viewport 2
-    "js/pages/homepage/viewport_2/HomepageGallery.js",
-    
-    //Viewport 3
-    "js/pages/homepage/viewport_3/homepage_about_animation.js",
-    
-    //Config files
-    "js/config/homepage/gallery_tiles/gallery_tiles26.js"
-  ],
+  js_files: [],
   
   defines: {
     common: {

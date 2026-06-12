14af91ea7199e4ef910644aa12cb8d0b1dcad5ee
Vis Tacitus <vistacitus@confoederatio.org>
2026-06-12 02:34:10 +0000

    Update config_backend.js

--- js/config/config_backend.js
+++ js/config/config_backend.js
@@ -44,59 +44,61 @@ config.homepage = {
     "js/config/homepage/gallery_tiles/gallery_tiles26.js"
   ],
   
-  defines: {},
-  
-  //Config parsers
-  banner: {
-    selectors: {
-      //Viewport 1
-      viewport_one: {
-        //Element selectors
-        about_body_container: document.getElementById("about-body-container"),
-        about_me_overlay_title: document.getElementById("about-me-overlay-title"),
-        about_me_overlay_subtitle: document.getElementById("about-me-overlay-subtitle"),
-        biography_overlay_subtitle: document.getElementById("biography-overlay-subtitle")
-      },
-      
-      //Viewport 2
-      viewport_two: {
-        //Core elements
-        gallery_width: 500, //Gallery width in vh
-        scene: document.getElementById("scene"),
+  defines: {
+    common: {
+      selectors: {
+        //Viewport 1
+        viewport_one: {
+          //Element selectors
+          about_body_container: document.getElementById("about-body-container"),
+          about_me_overlay_title: document.getElementById("about-me-overlay-title"),
+          about_me_overlay_subtitle: document.getElementById("about-me-overlay-subtitle"),
+          biography_overlay_subtitle: document.getElementById("biography-overlay-subtitle")
+        },
         
-        //Bookmark selectors
-        bookmark_container: document.getElementById("project-parallax-bookmark-container"),
-        bookmark_label: document.getElementById("project-parallax-bookmark-labels-container"),
-        bookmark_minimise_btn: document.getElementById("project-parallax-bookmark-minimise-icon"),
-        bookmark_scroll_x: 0,
-        bookmark_preview_container: document.getElementById("project-parallax-preview-container"),
-        bookmark_no_label: document.getElementById("project-parallax-no-bookmark-label"),
-        bookmark_old_index: 1,
-        bookmark_selected: "",
-        bookmark_items: [],
-        no_bookmark_label: document.getElementById("project-parallax-no-bookmark-label"),
+        //Viewport 2
+        viewport_two: {
+          //Core elements
+          gallery_width: 500, //Gallery width in vh
+          scene: document.getElementById("scene"),
+          
+          //Bookmark selectors
+          bookmark_container: document.getElementById("project-parallax-bookmark-container"),
+          bookmark_label: document.getElementById("project-parallax-bookmark-labels-container"),
+          bookmark_minimise_btn: document.getElementById("project-parallax-bookmark-minimise-icon"),
+          bookmark_scroll_x: 0,
+          bookmark_preview_container: document.getElementById("project-parallax-preview-container"),
+          bookmark_no_label: document.getElementById("project-parallax-no-bookmark-label"),
+          bookmark_old_index: 1,
+          bookmark_selected: "",
+          bookmark_items: [],
+          no_bookmark_label: document.getElementById("project-parallax-no-bookmark-label"),
+          
+          //Content panel selectors
+          content_panel_container: document.getElementById("main-parallax-content-panel-wrapper"),
+          content_panel_scroll_container: document.getElementById("main-parallax-content-panel-scroll-wrapper"),
+          content_panel_update_paused: false,
+          
+          //Parallax selectors
+          parallax_body: document.getElementById("project-parallax-container"),
+          parallax_container: document.getElementById("project-parallax-scroll-container"),
+          parallax_buttons: document.getElementById("project-parallax-dots-container"),
+          parallax_scroll_indicator: document.getElementById("project-parallax-scroll-fill-indicator"),
+          parallax_current_scroll_x: 0,
+          parallax_scroll_x: 0,
+          parallax_selected: [],
+          parallax_pinned_items: []
+        },
         
-        //Content panel selectors
-        content_panel_container: document.getElementById("main-parallax-content-panel-wrapper"),
-        content_panel_scroll_container: document.getElementById("main-parallax-content-panel-scroll-wrapper"),
-        content_panel_update_paused: false,
-        
-        //Parallax selectors
-        parallax_body: document.getElementById("project-parallax-container"),
-        parallax_container: document.getElementById("project-parallax-scroll-container"),
-        parallax_buttons: document.getElementById("project-parallax-dots-container"),
-        parallax_scroll_indicator: document.getElementById("project-parallax-scroll-fill-indicator"),
-        parallax_current_scroll_x: 0,
-        parallax_scroll_x: 0,
-        parallax_selected: [],
-        parallax_pinned_items: []
-      },
-      
-      //Viewport 3
-      viewport_three: {
-        
+        //Viewport 3
+        viewport_three: {
+          
+        }
       }
     }
   },
+  
+  //Config parsers
+  banner: {},
   gallery: {},
 };
